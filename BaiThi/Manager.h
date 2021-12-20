7d26a779fb835d405cf623d49b3437fa59b3d4a5
Duy200403 <92080512+Duy200403@users.noreply.github.com>
2021-12-20 22:52:39 +0700

    Initial commit

---
+++ BaiThi/Manager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Employee.h"
+
+class Manager : public Employee
+{
+private:
+	int m_yoe{};
+
+public:
+	Manager();
+
+	Manager(int id, string name, int age);
+
+	void input() override;
+
+	void show() override;
+};
+
+
